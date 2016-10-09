bfead994fa7ed9edddbb7b756bd4b9855a536941
voodmania <voodmania@gmail.com>
2016-10-09 15:15:27 +0200

    fixed missing parameter

--- bot.py
+++ bot.py
@@ -39,6 +39,7 @@ def webhook():
         process_msg(msg[key])
     return 'OK'  # allways ack telegram msg
 
+
 def process_msg(msg):
     flavor = telepot.flavor(msg)
     if flavor == 'callback_query':
@@ -46,6 +47,7 @@ def process_msg(msg):
     elif flavor == 'chat' and is_start_cmd(msg):
         chat_id = msg['chat']['id']
         bot.sendMessage(chat_id, START_MSG, reply_markup=get_days_keyboard())
+
 
 def process_callback(msg):
     bot.answerCallbackQuery(msg['id'])  # ack callback
@@ -56,16 +58,18 @@ def process_callback(msg):
         kb = get_days_keyboard()
         bot.sendMessage(chat_id, DAYS_MSG, reply_markup=kb)
     elif data[0] == 'schedules':
-        process_schedules(data[1])
+        process_schedules(chat_id, data[1])
     elif data[0] == 'slots':
-        process_slots(data[1], data[2])
+        process_slots(chat_id, data[1], data[2])
 
-def process_schedules(day):
+
+def process_schedules(chat_id, day):
     text = SCHEDULE_MSG.format(get_day_name(day))
     kb = get_schedules_keyboard(day)
     bot.sendMessage(chat_id, text, reply_markup=kb)
 
-def process_slots(day, start):
+
+def process_slots(chat_id, day, start):
     slots = [s for s in agenda.get_slots(day, start) if s.name]
     if len(slots):
         header_text = SLOTS_MSG.format(get_day_name(day), start)
@@ -77,13 +81,16 @@ def process_slots(day, start):
                 disable_web_page_preview=True, parse_mode='markdown',
                 reply_markup=kb)
 
+
 def is_start_cmd(msg):
     return 'text' in msg and msg['text'] == '/start'
+
 
 def get_day_name(day):
     for d in DAYS:
         if d.value == day:
             return d.name
+
 
 def get_slot_text(slot):
     text = u'*{}*\n'.format(slot.track) if slot.track else u''
@@ -91,14 +98,19 @@ def get_slot_text(slot):
     text += u'_{}_'.format(slot.speakers) if slot.speakers else u''
     return text.strip()
 
+
 def get_all_days_keyboard():
-    return InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text=INIT_MSG, callback_data='days')]])
+    return InlineKeyboardMarkup(inline_keyboard=[[
+        InlineKeyboardButton(text=INIT_MSG, callback_data='days')]])
+
 
 def get_days_keyboard():
     kb = []
     for day in DAYS:
-        kb.append([InlineKeyboardButton(text=day.name, callback_data='schedules {}'.format(day.value))])
+        kb.append([InlineKeyboardButton(text=day.name,
+            callback_data='schedules {}'.format(day.value))])
     return InlineKeyboardMarkup(inline_keyboard=kb)
+
 
 def get_schedules_keyboard(day):
     kb = []
